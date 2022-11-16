b877786a24108fb6fa0467465fd2e9dda7fd693d
timohuennebeck <hello@timohuennebeck.com>
2022-11-16 21:28:12 +0100

    connect to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-    client: "mysql2",
-    connection: {
-        host: "127.0.0.1",
-        user: "root",
-        password: "JL:1590!aus",
-        database: "jarvis_cap",
-        charset: "utf8",
-    },
+  client: "mysql2",
+  connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+  }
 };
